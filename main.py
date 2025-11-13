87f072573b9de6f858fb1c3c1200492ca685caff
ismail-shehata <ismail.shehata@edu.esiee.fr>
2025-11-13 09:20:50 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-from math import sqrt
-
-#### Fonction secondaire
+"""Module pour trouver et afficher les nombres premiers."""
+from math import isqrt
 
 
 def isprime(p):
+    """Vérifie si un nombre est premier."""
+    if p < 2:
+        return False
+    if p == 2:
+        return True
+    if p % 2 == 0:
+        return False
 
-    # votre code ici
-
-    pass
-
-#### Fonction principale
+    # Vérifier seulement les nombres impairs jusqu'à sqrt(p)
+    for i in range(3, isqrt(p) + 1, 2):
+        if p % i == 0:
+            return False
+    return True
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
-    for n in range(100):
-        if isprime(n):
-            print(n, end=", ")
-
-    print()
+    """Affiche tous les nombres premiers jusqu'à 100."""
+    primes = [n for n in range(2, 100) if isprime(n)]
+    print(", ".join(map(str, primes)) + ", ")
 
 
 if __name__ == "__main__":
